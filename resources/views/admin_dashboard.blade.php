45df41f2afd64b917d2db6fb0aeca93389542dfc
sahrul233 <arifshafrullah8@gmail.com>
2025-05-15 07:47:57 +0700

    Sahrul

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -6,7 +6,6 @@
             >
               <div>
                 <h3 class="fw-bold mb-3">Dashboard</h3>
-                <h6 class="op-7 mb-2">Free Bootstrap 5 Admin Dashboard</h6>
               </div>
               <div class="ms-md-auto py-2 py-md-0">
                 <a href="#" class="btn btn-label-info btn-round me-2">Manage</a>
@@ -618,4 +617,5 @@
                 </div>
             </div>
         </div>
+    </div>
 @endsection
